2cd1acdc2d3bf5aba520da817a5c663f1f214466
john mbugua <johnymbugua1997@gmail.com>
2023-02-11 08:16:41 +0300

    tailwind hotel project javascript and output.css file

---
+++ lesson3.js
@@ -0,0 +1,32 @@
+const menuEl = document.getElementById('menu_el');
+const menuBar = document.getElementById('menu_bar');
+const menuEx = document.getElementById('menu_ex');
+let widthx = window.innerWidth;
+function showMenu() {
+    if (menuEl.classList.contains('hidden')) {
+        menuEl.classList.remove('hidden');
+    } else {
+        menuEl.classList.add('hidden');
+    }
+    if (menuBar.classList.contains('block')) {
+        menuBar.classList.remove('block');
+
+    } else {
+        menuBar.classList.add('hidden');
+        menuEx.setAttribute('class', 'md:hidden block');
+    }
+}
+
+function showMenubar() {
+    if (menuEx.classList.contains('block') && menuBar.classList.contains('hidden')) {
+        menuEx.classList.remove('block');
+        menuBar.classList.remove('hidden');
+        menuEl.classList.add('hidden');
+        menuEx.classList.add('hidden');
+    }
+
+
+}
+
+menuEx.addEventListener('click', showMenubar, false);
+menuBar.addEventListener('click', showMenu, false);
